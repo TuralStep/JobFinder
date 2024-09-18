6bae5b27d7181e7ce0db48cc5472a5ca7252d15c
Haci_of80 <Haci_of80@STHQ0123-21>
2024-09-18 17:42:07 +0400

    Created VacancyController

---
+++ WebApi/Controllers/VacancyController.cs
@@ -0,0 +1,23 @@
+﻿using Application.Repositories.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VacancyController : ControllerBase
+    {
+
+        private readonly IVacancyRepository _repo;
+
+        public VacancyController(IVacancyRepository repo)
+        {
+            _repo = repo;
+        }
+
+
+        
+    }
+}
